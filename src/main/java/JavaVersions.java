2568288a30c206b1ce5d05de7fd8ffbb6151e6ae
Artin Simons <coder211@163.com>
2021-01-24 10:55:13 +0800

    init project

---
+++ src/main/java/JavaVersions.java
@@ -0,0 +1,6 @@
+public class JavaVersions {
+    public static void main(String[] args) {
+        System.out.println("jdk标准版本号: " + System.getProperty("java.specification.version"));
+        System.out.println("jdk详细版本号: " + System.getProperty("java.version"));
+    }
+}
